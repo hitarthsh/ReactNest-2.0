41992675486951a60bac15b996654c7338fd3a5b
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-07-30 22:13:22 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-2xl text-amber-600 flex justify-center items-center bg-cyan-400' >App</div>
+  )
+}
+
+export default App
